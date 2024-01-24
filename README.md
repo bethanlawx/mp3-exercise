2618db90e59ba958ab58d4826d73fea55f4ebc19
Beth 3 <beth.law@capgemini.com>
2024-01-24 15:48:20 +0000

    Added mp3 file processing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-TODO: change gts to prettier
+This is an API which allows the user to POST an MP3 file to the URL /file-header, and have the number of frames returned to them.
+
+Run 
+```npm run dev```
+to start the development environment 
+
+Run 
+```npm run start```
+to start the prod environment
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import express, { Express, Request, Response } from 'express';
-
+import processMp3File from './processMp3File';
 const app: Express = express();
 
 app.get('/health', (req: Request, res: Response) => {
   res.send('healthy <3');
+});
+
+app.post('/file-upload', async (req: Request, res: Response) => {
+  // TODO: check it is a MP3 file
+  var data = Buffer.from('binary');
+  req.on('data', (chunk) => {
+    data = Buffer.concat([data, chunk])
+  });
+  await req.on('end', async function() {
+    let frameCount = await processMp3File(data);
+    res.status(200).send({'frameCount': frameCount});
+  });
+
+  //TODO: Improve error handling
+  req.on('error', function(err) {
+    console.log(err.message);
+    res.status(500).send('Error has occurred during file processing');
+  });
 });
 
 export default app;
