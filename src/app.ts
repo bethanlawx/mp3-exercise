109eff04ec0a056689eed5810abbed099453bd21
Beth 3 <beth.law@capgemini.com>
2024-01-24 15:50:25 +0000

    Remove unnecessary async handling

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,14 @@ app.get('/health', (req: Request, res: Response) => {
   res.send('healthy <3');
 });
 
-app.post('/file-upload', async (req: Request, res: Response) => {
+app.post('/file-upload', (req: Request, res: Response) => {
   // TODO: check it is a MP3 file
   var data = Buffer.from('binary');
   req.on('data', (chunk) => {
     data = Buffer.concat([data, chunk])
   });
-  await req.on('end', async function() {
-    let frameCount = await processMp3File(data);
+  req.on('end', function() {
+    let frameCount = processMp3File(data);
     res.status(200).send({'frameCount': frameCount});
   });
 
